451482e510ce058c7fc421f9fd7aa26fe6ba6ea8
Low Jian Sheng <lowjiansheng@hotmail.sg>
2016-10-06 20:18:23 +0800

    test

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ var token = '291482532:AAEkKodHdD8E_tos79TlSbsW1y2ymkkaiek';
 // Setup polling way
 var bot = new TelegramBot(token, {polling: true});
 
+// Matches /echo [whatever]
+bot.onText(/\/whattime (.+)/, function (msg, match) {
+    var fromId = msg.from.id;
 
-// Any kind of message
-bot.onText('message', function (msg) {
-    var chatId = msg.chat.id;
-    bot.sendMessage(chatId, "hi");
+    bot.sendMessage(fromId, "hi test");
 });
